33bc651e813a28ec867f62f2ed9cc7f9df1f27df
martinskarbala <martin@supernova.io>
2026-09-16 14:56:59 +0200

    updated config, fixed test

--- tests/quoteGenerator.spec.ts
+++ tests/quoteGenerator.spec.ts
@@ -35,39 +35,3 @@ test.describe("Potter Quotes", () => {
     );
   });
 });
-
-test.describe("Potter Quotes", () => {
-  test.beforeEach("open page", async ({ page }) => {
-    await page.goto("http://localhost:8080/#/quotes");
-  });
-  test("display quote", async ({ page }) => {
-    await page.locator('[data-test="get-quote"]').click();
-    await expect(page.locator("ul.quote-list").locator("li")).toBeVisible();
-    await expect(page.locator("ul.quote-list").locator("li")).not.toBeEmpty();
-  });
-  test("title should be correct", async ({ page }) => {
-    await expect(page.locator("h1.title")).toHaveText("Potter Quotes");
-    await expect(
-      page.getByRole("heading", { name: "Potter Quotes" }),
-    ).toBeVisible();
-  });
-  test("remove quote is disabled on page open", async ({ page }) => {
-    await expect(
-      page.getByRole("button", { name: "Remove Quote" }),
-    ).toBeDisabled();
-  });
-  test("empty list should have a message displayed", async ({ page }) => {
-    await expect(
-      page.getByText("Click the button to get some wisdom", { exact: true }),
-    ).toBeVisible();
-  });
-  test("wisdom points are updated on button click", async ({ page }) => {
-    await page.locator('[data-test="get-quote"]').click();
-    await expect(page.locator('[data-test="wisdom-points"]')).toContainText(
-      "+1",
-    );
-    await expect(page.locator('[data-test="wisdom-points"]')).toHaveText(
-      "wisdom points +1",
-    );
-  });
-});
