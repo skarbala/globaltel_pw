e6e2890654e1b3440403290768ec6ab995fd9dc7
martinskarbala <martin@supernova.io>
2026-09-16 14:47:45 +0200

    tests update

---
+++ tests/gringottBank.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+test("create an offer with correct data", async ({ page }) => {
+  const fund = "Death Eater Dominance Fund";
+
+  await page.goto("http://localhost:8080/#/gringottsBank");
+  // page.locator("[id=selectedFund]");
+
+  await page.locator("#selectedFund").selectOption(fund);
+  await page.locator("#oneTimeInvestment").fill("25000");
+  await page.locator("#years").fill("20");
+  //  await page.getByRole("button", { name: "Make me an offer" }).click();
+  await page.locator('[data-test="create-offer"]').click();
+
+  const offerDetail = page.locator("div.offer-detail");
+  await expect(offerDetail.locator("div.your-data")).toBeVisible();
+  await expect(offerDetail.locator("div.your-data p.fund span")).toHaveText(
+    fund,
+  );
+  //samostatne overit pocet rokov
+  await expect(
+    offerDetail.locator("div.your-data").locator("p.period").locator("span"),
+  ).toHaveText("20 years");
+});
